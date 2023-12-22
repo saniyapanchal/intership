4efc57939def9531cdaa7807bc6f372d7dda0eaa
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 19:29:54 +0530

    Delete d2.sql

--- d2.sql
+++
@@ -1,6 +0,0 @@
-create table student (name varchar(50), Rollno int(15), state varchar(50));
-insert into student values ('Joy', 120, 'Mumbai');
-insert into student values ('Dax', 118, 'Mumbai');
-insert into student values ('Rashida', 120, 'Delhi');
-insert into student values ('Helly', 211, 'Ahmedabad');
-insert into student values ('Saniya', 200, 'Ahmedabad');
