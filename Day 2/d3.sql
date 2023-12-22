403d07c7ffaeb2fec01af90d6cd4d2c09178620f
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 19:33:24 +0530

    Day 2

---
+++ Day 2/d3.sql
@@ -0,0 +1,6 @@
+create table shopping ( C_name varchar(50), Buy varchar(50), Shop varchar(50));
+insert into shopping values ('Joy', 'Food' , 'Mumbai');
+insert into shopping values ('Dax', 'clothes', 'Mumbai');
+insert into shopping values ('Rashida', 'makeup', 'Delhi');
+insert into shopping values ('Helly', 'notebook', 'Ahmedabad');
+insert into shopping values ('Saniya', 'Food', 'Ahmedabad');
