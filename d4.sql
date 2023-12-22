158f487ee9dfbf25189650d210e8e3c8d6cb4c90
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 19:30:19 +0530

    Delete d4.sql

--- d4.sql
+++
@@ -1,5 +0,0 @@
-create table myfamily ( name varchar(50), profession varchar(50), hobby varchar(50), realtion varchar(50));
-insert into myfamily values ('Rakesh', 'Accounting' , 'Travelling' , 'Father');
-insert into myfamily values ('Aarti', 'Housewife', 'Sports' , 'Mother');
-insert into myfamily values ('Rushil', 'Student', 'Studing' , 'Brother');
-insert into myfamily values ('Saniya', 'Student', 'Gaming' , 'me');
