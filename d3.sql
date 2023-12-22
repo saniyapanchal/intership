2b47f8c19c77eb4063e627191b91b0b0fc23c62f
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 19:29:25 +0530

    Delete d3.sql

--- d3.sql
+++
@@ -1,6 +0,0 @@
-create table shopping ( C_name varchar(50), Buy varchar(50), Shop varchar(50));
-insert into shopping values ('Joy', 'Food' , 'Mumbai');
-insert into shopping values ('Dax', 'clothes', 'Mumbai');
-insert into shopping values ('Rashida', 'makeup', 'Delhi');
-insert into shopping values ('Helly', 'notebook', 'Ahmedabad');
-insert into shopping values ('Saniya', 'Food', 'Ahmedabad');
