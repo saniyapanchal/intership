d6aeea0c0eedf16f52a05f515224d347099034ce
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 19:29:37 +0530

    Delete d1.sql

--- d1.sql
+++
@@ -1,6 +0,0 @@
-create table bank ( name varchar(50), amount int(15), branch varchar(50));
-insert into bank values ('Joy', 50000 , 'BOI');
-insert into bank values ('Dax', 50000 , 'IOB');
-insert into bank values ('Rashida', 30000 , 'SBI');
-insert into bank values ('Helly', 20000 , 'HDFC');
-insert into bank values ('Saniya', 20000 , 'IOB');
