333fb941e801d3f30cbb8a7d677a305290fcf2be
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 19:30:32 +0530

    Delete d5.sql

--- d5.sql
+++
@@ -1,6 +0,0 @@
-create table books ( name varchar(50), price int(50), sell int(500));
-insert into books values ('Textbook', 40, 100);
-insert into books values ('Singleline', 20 , 200);
-insert into books values ('Maths', 25 , 150);
-insert into books values ('Drawing', 35, 50);
-insert into books values ('Diary', 45 , 20);
